024c23ef84139dc6cc9f2362c049acc5946ca286
Jason Kölker <jason@koelker.net>
2019-09-27 20:50:51 +0000

    BUG: handle unknown stream id

--- src/mux_h2.c
+++ src/mux_h2.c
@@ -2589,6 +2589,8 @@ static void h2_process_demux(struct h2c *h2c)
 
 		case H2_FT_HEADERS:
 			if (h2c->st0 == H2_CS_FRAME_P) {
+				if (!h2s)
+					goto fail;
 				if (h2c->flags & H2_CF_IS_BACK)
 					tmp_h2s = h2c_bck_handle_headers(h2c, h2s);
 				else
